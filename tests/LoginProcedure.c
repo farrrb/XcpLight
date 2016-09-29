798dac3d62454b9b968e3aa79ef8c2a32fed5f27
0xFAB <fabian.zahn@gmail.com>
2016-09-29 20:12:52 +0200

    setUp & tearDown integration (Unity)

--- tests/LoginProcedure.c
+++ tests/LoginProcedure.c
@@ -6,6 +6,47 @@
 #include "XcpLight.h"
 #include "XcpLight_internals.h"
 
+extern XcpLightMessage_t replyMsg;
+extern XcpLightInternals_t _XcpLightData;
+
+XcpLightMessage_t cmdMsg = {0};
+
+/* santa's little helper functions */
+
+void clearCmdMessage(void)
+{
+  int i;
+  for(i=0; i<XCPLIGHT_CFG_XTO_LENGTH; i++)
+  {
+    cmdMsg.payload[i] = 0u;
+  }
+  cmdMsg.length = 0u;
+}
+
+void clearReplyMessage(void)
+{
+  int i;
+  for(i=0; i<XCPLIGHT_CFG_XTO_LENGTH; i++)
+  {
+    replyMsg.payload[i] = 0u;
+  }
+  replyMsg.length = 0u;
+}
+
+/* test setUp / tearDown */
+
+void setUp(void)
+{
+  clearCmdMessage();
+  clearReplyMessage();
+}
+
+void tearDown(void)
+{
+}
+
+/* the actual tests */
+
 void test_LoginProcedureDummy(void)
 {
   TEST_ASSERT_TRUE(1);
