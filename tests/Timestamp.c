52ca475d467457bcf077e51a87b4c2b917f0e5b9
0xFAB <fabian.zahn@gmail.com>
2016-09-30 12:28:06 +0200

    Add timestamp tests

---
+++ tests/Timestamp.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "unity.h"
+
+#include "XcpLight.h"
+#include "XcpLight_internals.h"
+
+extern XcpLightMessage_t replyMsg;
+extern XcpLightInternals_t _XcpLightData;
+
+XcpLightMessage_t cmdMsg = {0};
+
+/* santa's little helper functions */
+
+void clearCmdMessage(void)
+{
+  int i;
+  for(i=0; i<XCPLIGHT_CFG_XTO_LENGTH; i++)
+  {
+    cmdMsg.payload[i] = 0u;
+  }
+  cmdMsg.length = 0u;
+}
+
+void clearReplyMessage(void)
+{
+  int i;
+  for(i=0; i<XCPLIGHT_CFG_XTO_LENGTH; i++)
+  {
+    replyMsg.payload[i] = 0u;
+  }
+  replyMsg.length = 0u;
+}
+
+/* test setUp / tearDown */
+
+void setUp(void)
+{
+  clearCmdMessage();
+  clearReplyMessage();
+}
+
+void tearDown(void)
+{
+}
+
+/* the actual tests */
+
+void test_Template(void)
+{
+  TEST_ASSERT_TRUE(1);
+}
+
+int main(void)
+{
+  UNITY_BEGIN();
+  RUN_TEST(test_Template);
+  return UNITY_END();
+}
